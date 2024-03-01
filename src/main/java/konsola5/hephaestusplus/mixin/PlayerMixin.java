e1c7578c1e1e404cd8f55863bef281548ffc19d4
KonSola5 <125081901+KonSola5@users.noreply.github.com>
2024-03-01 17:57:31 +0100

    Make PlayerMixin apply before Porting Lib does to avoid random chance of crash on startup

--- src/main/java/konsola5/hephaestusplus/mixin/PlayerMixin.java
+++ src/main/java/konsola5/hephaestusplus/mixin/PlayerMixin.java
@@ -7,10 +7,11 @@ import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(Player.class)
+@Mixin(value = Player.class, priority = 1001)
 public class PlayerMixin {
     /**
      * Registers the Carmot Shield attribute.
+     * Priority 1001, since Porting Lib doesn't use @ModifyReturnValue.
      */
     @ModifyReturnValue(method = "createAttributes", at = @At("RETURN"))
     private static AttributeSupplier.Builder hephaestusPlus$addCarmotShieldAttribute(AttributeSupplier.Builder original) {
